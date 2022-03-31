e6d6197218dcddf40e4d6011019e46de7396c272
Kristina Palmer <Kristina.Palmer@vu.com>
2022-03-31 13:32:48 -0500

    5th Attempt

--- displayInventory.py
+++ displayInventory.py
@@ -3,12 +3,13 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 def displayInventory(inventory):
 # your code goes here
     print('Inventory:')
-    
-    for key, value in stuff.items(): #for loop to go through each key/value
-        print(value, key) #print in reverse order
-    print('Total number of items:',sum(stuff.values()))
 
-#displayInventory(stuff)
+displayInventory(stuff)
+    
+for key, value in stuff.items(): #for loop to go through each key/value
+    print(value, key) #print in reverse order
+print('Total number of items:',sum(stuff.values()))
+   
 
 
 
